6e86349c9a73366ede65a204a0361eb51fcb5f35
Alex <Alex@DESKTOP-6TFN45U>
2021-10-22 22:17:45 +0300

    Добавьте файлы проекта.

---
+++ String_Builder/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "двадцять людей йдуть по широкому шляху. Вони людей спiвають старовинних пiсень, та пiдганяють гужових волiв.";
+                       
+            StringBuilder rez = new StringBuilder("");
+            
+            Console.WriteLine(a);
+
+           Console.WriteLine($"введите слово");
+           string K = Console.ReadLine();
+
+            int prognose_vhojd = 1;
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+                prognose_vhojd = FindWordInText(a, K, prognose_vhojd);
+            }
+
+
+            string[] mas = {"ють","уть","ять","ать","ати","ве"};
+            glagol(a, mas, rez);
+    }
+
+
+        public static void glagol(string a, string[] mas, StringBuilder rez)
+        {
+            bool flag = false;
+            string[] z = a.Split();
+
+            foreach (string r in z)
+            {
+                flag = false;
+                foreach (string slv in mas)
+                    if (r.Contains(slv) && !r.Contains("цять"))
+                    {
+                        flag = true;
+                        Console.WriteLine(r);
+                        break;
+                    }
+
+                if (!flag) { rez.Append(r + " "); }
+            }
+
+            Console.WriteLine($"в итоге строка: {rez}");
+        }
+
+        public static int FindWordInText(string stroka, string word, int prognose_vhojd)
+        {
+            string[] mas_slov = stroka.Split();
+            int vhojd = 0, granich_vhojd=0;
+            
+            foreach (string slov in mas_slov)
+            {
+                if (string.Compare(slov, word) == 0)
+                    granich_vhojd++;
+            }
+
+            if (granich_vhojd < prognose_vhojd || prognose_vhojd >= mas_slov.Length)
+            {
+                Console.WriteLine("there are no much this word in string");
+                return 1;                
+            }
+
+                foreach (string slov in mas_slov)
+                {
+                if (string.Compare(slov, word) == 0 && ++vhojd == prognose_vhojd)
+                    {
+                       Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write(slov + " ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(slov + " ");
+                    }
+                }
+                Console.WriteLine();
+
+                return ++prognose_vhojd;            
+        }     
+    }
+}
---
+++ Tochki/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MnogPoints A = new MnogPoints();
+
+        foreach (Point element in A.points)
+        {
+            Console.Write($"точка № {element.coord.num}: ");
+            Console.Write($"x={element.coord.x} ");
+            Console.Write($"y={element.coord.y} \n\n");
+        }
+
+        Point P = new Point();
+        Console.WriteLine($"введите абсциссу (х)");
+        P.coord.x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"введите ординату (у)");
+        P.coord.y = Convert.ToInt32(Console.ReadLine());
+
+        //точка наиболее приближенная к Р
+        double range = 0; ;
+        int tochka = 0;
+        near_point(P, tochka, A, range);
+
+        //точка наиболее удаленная от Р
+        far_point(P, tochka, A, range);
+
+        same_line(A, tochka);
+    }
+
+    public static void near_point(Point P, int tochka, MnogPoints A, double range)
+    {
+        double ab;
+        range = double.MaxValue;
+        int x1 = P.coord.x, y1 = P.coord.y, x2, y2, a, b, c;
+        foreach (Point element in A.points)
+        {
+            x2 = element.coord.x;
+            y2 = element.coord.y;
+
+            a = (x2 - x1) * (x2 - x1);
+            b = (y2 - y1) * (y2 - y1);
+            c = a + b;
+            ab = Math.Sqrt(c);
+
+            if (range > ab)
+            {
+                range = ab;
+                tochka = element.coord.num;
+            }
+        }
+        Console.WriteLine($"наиболее приближенная точка это {tochka}-ая");
+
+    }
+    public static void far_point(Point P, int tochka, MnogPoints A, double range)
+    {
+        double ab;
+        int x1 = P.coord.x, y1 = P.coord.y, x2, y2, a, b, c;
+        range = double.MinValue;
+        foreach (Point element in A.points)
+        {
+            x2 = element.coord.x;
+            y2 = element.coord.y;
+
+            a = (x2 - x1) * (x2 - x1);
+            b = (y2 - y1) * (y2 - y1);
+            c = a + b;
+            ab = Math.Sqrt(c);
+
+            if (range < ab)
+            {
+                range = ab;
+                tochka = element.coord.num;
+            }
+        }
+        Console.WriteLine($"наиболее удаленная точка это {tochka}-ая");
+    }
+    public static void same_line(MnogPoints A, int tochka) //точки из множества которые лежат на одной прямой с заданной прямой
+    {
+
+        // A x +B y + C = 0
+        Double I, J, K;
+        Console.WriteLine($"введите I");
+        I = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"введите J");
+        J = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"введите K");
+        K = Convert.ToDouble(Console.ReadLine());
+
+
+        foreach (Point element in A.points)
+        {
+            if (I * element.coord.x + J * element.coord.y + K == 0)
+            {
+                tochka = element.coord.num;
+                Console.WriteLine($"точка {tochka}-ая лежит на заданной линии");
+            }
+        }
+    }
+}
+
+public struct Point
+{
+    public (int num, int x, int y) coord;
+}
+
+class MnogPoints
+{
+    public Point[] points = new Point[5];
+
+
+
+    public MnogPoints()
+    {
+        var rnd = new Random();
+        for (int i = 0; i < 5; i++)
+        {
+            points[i].coord.num = i;
+            points[i].coord.x = rnd.Next(-40, 40);
+            points[i].coord.y = rnd.Next(-40, 40);
+        }
+    }
+}
